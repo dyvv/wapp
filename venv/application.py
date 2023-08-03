c2a9996205862450130dc2700db64c513a8fea03
Dmitry Yurov <dmitry.yurov@transwestern.com>
2023-08-03 15:36:38 -0300

    Third commit

--- venv/application.py
+++ venv/application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Good day! Best Bike App!</h1></body></html>\n"
+    return "<html><body><h1>Good day! Ha Ha! Best Bike App!</h1></body></html>\n"
